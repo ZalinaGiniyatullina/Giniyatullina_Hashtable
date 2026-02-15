dfa9d41c91bbb46cadf4adce258f5cf0ecb9b74d
ZalinaGiniyatullina <zalinaginiatullina@gmail.com>
2026-02-15 19:41:15 +0500

    first commit

---
+++ Giniyatullina_Hashtable/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Giniyatullina_Hashtable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Фамилия:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Телефон:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 69);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Удалить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(625, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(294, 255);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(229, 69);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Найти телефон по фамилии";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(590, 255);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(243, 69);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Найти фамилию по телефону";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(360, 391);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Результат:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(72, 419);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(742, 204);
+            this.listBox1.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(208, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 34);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(201, 138);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(338, 34);
+            this.textBox2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 674);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Записная книжка";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Giniyatullina_Hashtable/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Giniyatullina_Hashtable
+{
+    public partial class Form1 : Form
+    {
+        private Hashtable phoneBook = new Hashtable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdatePhoneList()
+        {
+            listBox1.Items.Clear();
+            foreach (DictionaryEntry entry in phoneBook)
+            {
+                listBox1.Items.Add($"{entry.Key}: {entry.Value}");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string surname = textBox1.Text.Trim();
+            string phone = textBox2.Text.Trim();
+            if (string.IsNullOrEmpty(surname) || string.IsNullOrEmpty(phone))
+            {
+                MessageBox.Show("Введите фамилию и телефон!", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (phoneBook.ContainsKey(surname))
+            {
+                MessageBox.Show("Запись с такой фамилией уже существует!", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            phoneBook.Add(surname, phone);
+            textBox1.Clear();
+            textBox2.Clear();
+            UpdatePhoneList();
+            MessageBox.Show("Запись добавлена!", "Успех",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string surname = textBox1.Text.Trim();
+
+            if (string.IsNullOrEmpty(surname))
+            {
+                MessageBox.Show("Введите фамилию для удаления!", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (phoneBook.ContainsKey(surname))
+            {
+                phoneBook.Remove(surname);
+                UpdatePhoneList();
+                textBox1.Clear();
+                MessageBox.Show("Запись удалена!", "Успех",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Запись с такой фамилией не найдена!", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string surname = textBox1.Text.Trim();
+
+            if (string.IsNullOrEmpty(surname))
+            {
+                MessageBox.Show("Введите фамилию для поиска!", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            listBox1.Items.Clear();
+
+            if (phoneBook.ContainsKey(surname))
+            {
+                string phone = phoneBook[surname].ToString();
+                listBox1.Items.Add($"Найдено: {surname} - {phone}");
+            }
+            else
+            {
+                listBox1.Items.Add($"Фамилия '{surname}' не найдена");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string phone = textBox2.Text.Trim();
+
+            if (string.IsNullOrEmpty(phone))
+            {
+                MessageBox.Show("Введите номер телефона для поиска!", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            listBox1.Items.Clear();
+            bool found = false;
+
+            foreach (DictionaryEntry entry in phoneBook)
+            {
+                if (entry.Value.ToString() == phone)
+                {
+                    listBox1.Items.Add($"Найдено: {entry.Value} - {entry.Key}");
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                listBox1.Items.Add($"Телефон '{phone}' не найден");
+            }
+        }
+    }
+}
